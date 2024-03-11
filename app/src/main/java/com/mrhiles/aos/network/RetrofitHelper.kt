21a310a0ffd0b325fdbee23e054140763b856fb2
backtrackrt5 <backtrackrt5@gmail.com>
2024-03-11 15:48:26 +0900

    add retrofit  - retrofit package  - converter-gson  - converter-scalars  - retrofit constructor class  - retrofit interface

---
+++ app/src/main/java/com/mrhiles/aos/network/RetrofitHelper.kt
@@ -0,0 +1,18 @@
+package com.mrhiles.aos.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+class RetrofitHelper {
+    companion object {
+        fun getRetrofitInstance(baseurl:String) : Retrofit {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(baseurl)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit
+        }
+    }
+}
---
+++ app/src/main/java/com/mrhiles/aos/network/RetrofitService.kt
@@ -0,0 +1,14 @@
+package com.mrhiles.aos.network
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface RetrofitService {
+    //카카오 키워드로 장소 검색하기
+    // 스터디 장소를 확인하기 위한 api 요청. KakaoAK REST API Key
+    @Headers("Authorization KakaoAK e85398842cfcf70cda90f41067f11aa6")
+    @GET("/v2/local/search/keyword.json")
+    fun searchStudyRoomToString(@Query("query") query:String="스터디룸|스터디카페", @Query("x") longitute:String="", @Query("y") latitude:String="", @Query("radius") radius:Int=1000, @Query("page") page:Int=1,@Query("sort") sort:String="accuracy") : Call<String>
+}
