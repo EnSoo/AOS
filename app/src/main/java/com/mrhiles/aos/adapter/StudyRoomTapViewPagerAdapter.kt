345474ed0b168001420abe7761e9794d93447157
backtrackrt5 <backtrackrt5@gmail.com>
2024-03-12 11:45:32 +0900

    Add TabLayout to BottomHomeFragment.kt modify  - class name

--- app/src/main/java/com/mrhiles/aos/adapter/StudyRoomTapViewPagerAdapter.kt
+++ app/src/main/java/com/mrhiles/aos/adapter/StudyRoomTapViewPagerAdapter.kt
@@ -3,16 +3,16 @@ package com.mrhiles.aos.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.mrhiles.aos.fragments.BottomHomeTapFavorFragment
-import com.mrhiles.aos.fragments.BottomHomeTapListFragment
+import com.mrhiles.aos.fragments.TapHomeFavorFragment
+import com.mrhiles.aos.fragments.TapHomeListFragment
 
 class StudyRoomTapViewPagerAdapter(val fragmentActivity:FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
     override fun getItemCount()=2
     override fun createFragment(position: Int): Fragment {
         return when(position){
-            0 -> BottomHomeTapListFragment()
-            1 -> BottomHomeTapFavorFragment()
-            else -> BottomHomeTapListFragment()
+            0 -> TapHomeListFragment()
+            1 -> TapHomeFavorFragment()
+            else -> TapHomeListFragment()
         }
 
     }
--- app/src/main/java/com/mrhiles/aos/fragments/TapHomeFavorFragment.kt
+++ app/src/main/java/com/mrhiles/aos/fragments/TapHomeFavorFragment.kt
@@ -4,17 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.mrhiles.aos.activities.MainActivity
-import com.mrhiles.aos.adapter.StudyRoomTapListRecyclerAdapter
+import com.mrhiles.aos.adapter.StudyRoomTapFavorRecyclerAdapter
 import com.mrhiles.aos.databinding.FragmentBottomHomeBinding
-import com.mrhiles.aos.databinding.FragmentBottomHomeTapListBinding
+import com.mrhiles.aos.databinding.FragmentTapHomeFavorBinding
 
-class BottomHomeTapListFragment : Fragment(){
+class TapHomeFavorFragment : Fragment(){
     // kakao search API 응답결과 객체 참조변수
     //val documents:MutableList<StudyRoom> by lazy { mutableListOf() }
-    private val binding by lazy { FragmentBottomHomeTapListBinding.inflate(layoutInflater) }
+    private val binding by lazy { FragmentTapHomeFavorBinding.inflate(layoutInflater) }
     private val home_binding by lazy { FragmentBottomHomeBinding.inflate(layoutInflater) }
     private var searchQuery:String=""
     override fun onCreateView(
@@ -29,6 +28,6 @@ class BottomHomeTapListFragment : Fragment(){
         super.onViewCreated(view, savedInstanceState)
         val ma:MainActivity = activity as MainActivity
         ma.searchStudyRoomResponse ?: return // null일 경우 서버로딩이 완료되지 않았을 수도 있어서 리턴
-        binding.homeRecycler.adapter = StudyRoomTapListRecyclerAdapter(requireContext(), ma.searchStudyRoomResponse!!.documents)
+        binding.homeRecycler.adapter = StudyRoomTapFavorRecyclerAdapter(requireContext(), ma.searchStudyRoomResponse!!.documents)
     }
 }
