3af93614d61e3027e8afbe08393832477ac4a353
backtrackrt5 <backtrackrt5@gmail.com>
2024-03-12 10:31:44 +0900

    Set so that only study room users can wishlist

--- app/src/main/java/com/mrhiles/aos/adapter/StudyRoomRecyclerAdapter.kt
+++ app/src/main/java/com/mrhiles/aos/adapter/StudyRoomRecyclerAdapter.kt
@@ -3,10 +3,12 @@ package com.mrhiles.aos.adapter
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.google.gson.Gson
+import com.mrhiles.aos.G
 import com.mrhiles.aos.activities.StudyRoomDetailActivity
 import com.mrhiles.aos.data.StudyRoom
 import com.mrhiles.aos.databinding.RecyclerAdapterStudyRoomListBinding
@@ -23,7 +25,9 @@ class StudyRoomRecyclerAdapter(val context:Context, val documents:List<StudyRoom
         holder.binding.placeName.text=studyRoom.place_name
         holder.binding.tvCategory.text=studyRoom.category_name
         holder.binding.phoneNumber.text="+82 ${studyRoom.phone}"
-
+        // 로그인 한 상태여야만 찜 가능
+        if(G.isLogin) holder.binding.favor.visibility= View.VISIBLE
+        // StudyRoom 클릭 시 웹뷰 출력
         holder.binding.homeItem.setOnClickListener{
             val intent = Intent(context,StudyRoomDetailActivity::class.java)
 
