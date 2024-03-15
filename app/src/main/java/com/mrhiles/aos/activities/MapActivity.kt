20c40705bc2ae69e4517ab2c77b96c06c7f69868
backt <backtrackrt5@gmail.com>
2024-03-16 00:58:04 +0900

    MapActivity - 검색결과에 대해 동기화 처리 필요

--- app/src/main/java/com/mrhiles/aos/activities/MapActivity.kt
+++ app/src/main/java/com/mrhiles/aos/activities/MapActivity.kt
@@ -1,14 +1,20 @@
 package com.mrhiles.aos.activities
 
 import android.annotation.SuppressLint
-import android.graphics.PointF
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
 import com.mrhiles.aos.R
+import com.mrhiles.aos.data.KakaoSearchStudyRoomRespnose
+import com.mrhiles.aos.data.Meta
 import com.mrhiles.aos.data.StudyRoom
 import com.mrhiles.aos.databinding.ActivityMapBinding
+import com.mrhiles.aos.network.RetrofitHelper
+import com.mrhiles.aos.network.RetrofitService
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.MapFragment
@@ -18,11 +24,17 @@ import com.naver.maps.map.overlay.Align
 import com.naver.maps.map.overlay.InfoWindow
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.Overlay
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import kotlin.concurrent.thread
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     private val binding by lazy { ActivityMapBinding.inflate(layoutInflater) }
-    private lateinit var studyRoom:StudyRoom
     private lateinit var naverMap : NaverMap
+    private var location=""
+    private var distance=""
+    private var searchQuery=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -44,23 +56,129 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     @SuppressLint("ResourceAsColor")
     override fun onMapReady(naverMap: NaverMap) {
+        // naverMap.cameraPosition.target.also{it.y, it.x} 현재 카메라 위치 구하는 방법
         this.naverMap=naverMap
         val type=intent.getStringExtra("type")
+        val s:String?=intent.getStringExtra("studyRoom")
+        val studyRooms:MutableList<StudyRoom> = mutableListOf()
 
-        if(type=="Item") {
-            val s:String?=intent.getStringExtra("studyRoom")
-            s.also { studyRoom= Gson().fromJson(it, StudyRoom::class.java) }
-            val latLng= LatLng(studyRoom.y.toDouble(),studyRoom.x.toDouble())
-            //내 위치로 카메라 이동
-            val cameraUpdate = CameraUpdate.scrollAndZoomTo(latLng,15.0)
-            this.naverMap.moveCamera(cameraUpdate)
+
+        if(type=="Item") { // 1개의 아이템일 경우
+            val studyRoom= Gson().fromJson(s, StudyRoom::class.java)
+            studyRooms.add(studyRoom)
+        } else if (type=="list") {  // 아이템 리스트일 경우
+            val data=Gson().fromJson(s,Array<StudyRoom>::class.java)
+            data.forEach {
+              studyRooms.add(it)
+            }
+        }
+        setMarkerCameraMove(LatLng(studyRooms.get(0).y.toDouble(),studyRooms.get(0).x.toDouble()),studyRooms)
+    }
+
+    private fun dropDownSetting() {
+        // 드롭다운 지역 아이템 설정
+        val regionArray= resources.getStringArray(R.array.lecture_location_list)
+        val arrayAdapter=ArrayAdapter<String>(this,R.layout.lecture_dropdown_item, regionArray)
+        binding.dropdownMenu.setAdapter(arrayAdapter)
+
+        // 드롭다운 범위 아이템 설정
+        val distanceList= mutableListOf<String>()
+        distanceList.add("")
+        for(i in 10 until 300 step 10) {
+            distanceList.add(i.toString())
+        }
+        for(i in 300 until 2000 step 100) {
+            distanceList.add(i.toString())
+        }
+        for(i in 2000..20000 step 1000) {
+            distanceList.add(i.toString())
+        }
+        val distanceAdapter=ArrayAdapter<String>(this,R.layout.lecture_dropdown_item,distanceList)
+        binding.dropdownMenu2.setAdapter(distanceAdapter)
+        // 지역 드롭다운에서 아이템 클릭 시
+        binding.dropdownMenu.setOnItemClickListener { parent, view, position, id ->
+            location=(view as TextView).text.toString()
+            thread {
+                val lock = Any()
+                synchronized(lock) {
+                    val studyRooms=getStudyRooms()
+                    if(studyRooms.size != 0) { studyRooms.get(0).also { setMarkerCameraMove(LatLng(it.y.toDouble(), it.x.toDouble()), studyRooms) } }
+                }
+            }
+
+        }
+        // 범위 드롭다운에서 아이템 클릭 시
+        binding.dropdownMenu2.setOnItemClickListener { parent, view, position, id ->
+            distance=(view as TextView).text.toString()
+            thread {
+                val lock = Any()
+                synchronized(lock) {
+                    val studyRooms=getStudyRooms()
+                    if(studyRooms.size != 0) { studyRooms.get(0).also { setMarkerCameraMove(LatLng(it.y.toDouble(), it.x.toDouble()), studyRooms) } }
+                }
+            }
+        }
+        binding.inputEditer.setOnEditorActionListener { v, actionId, event ->
+            searchQuery=binding.inputEditer.text.toString()
+            thread {
+                val lock = Any()
+                synchronized(lock) {
+                    val studyRooms=getStudyRooms()
+                    if(studyRooms.size != 0) { studyRooms.get(0).also { setMarkerCameraMove(LatLng(it.y.toDouble(), it.x.toDouble()), studyRooms) } }
+                }
+            }
+
+            false
+        }
+    }
+    //검색 결과에 따라 studyroom list 출력
+    private fun getStudyRooms() :List<StudyRoom> {
+        val studyRooms:MutableList<StudyRoom> = mutableListOf()
+        val retrofit= RetrofitHelper.getRetrofitInstance("https://dapi.kakao.com")
+        val retrofitService=retrofit.create(RetrofitService::class.java)
+        var call:Call<KakaoSearchStudyRoomRespnose>
+        var documents:List<StudyRoom> = mutableListOf()
+        if(distance=="") distance="1000" //기본값 1000
+        Log.d("tset","${location} ${searchQuery} 스터디룸|스터디카페")
+        call=retrofitService.searchStudyRoomToString(radius=distance.toInt(), query = "${location} ${searchQuery} 스터디룸|스터디카페")
+        call.enqueue(object : Callback<KakaoSearchStudyRoomRespnose>{
+            override fun onResponse(
+                call: Call<KakaoSearchStudyRoomRespnose>,
+                response: Response<KakaoSearchStudyRoomRespnose>
+            ) {
+                val searchStudyRoomResponse=response.body()
+
+                searchStudyRoomResponse ?: return
+                documents= searchStudyRoomResponse!!.documents
+            }
+
+            override fun onFailure(call: Call<KakaoSearchStudyRoomRespnose>, t: Throwable) {
+                Toast.makeText(this@MapActivity, "서버 오류가 있습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+
+        return documents
+    }
+
+    // studyroom에 따라 마커 표시 및 카메라 이동
+    @SuppressLint("ResourceAsColor")
+    private fun setMarkerCameraMove(latLng:LatLng, studyRooms: List<StudyRoom>){
+        // 카메라 이동
+        val cameraUpdate = CameraUpdate.scrollAndZoomTo(latLng,15.0)
+
+        //내 위치로 카메라 이동
+        this.naverMap.moveCamera(cameraUpdate)
+
+        // 마커 및 정보창 처리
+        studyRooms.forEach{studyRoom ->
 
             // 마커 작업
             val maker=Marker()                    // 마커 객체 생성
             maker.apply {
                 position=latLng                   // 마커 위치지정
                 map=naverMap                      // 마커 지도 추가 삭제하려면 null
-                iconTintColor=R.color.mydefault   // 마커 틴트컬러
                 captionText=studyRoom.place_name  // 마커 캡션 텍스트
                 captionRequestedWidth=200         // 마커 캡션 텍스트 너비
                 setCaptionAligns(Align.Bottom)    // 마커 캡션 위치
@@ -91,30 +209,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             }
 
             maker.onClickListener=listener
-        } else if (type=="list") {
+        }
 
-        }
-    }
-
-    private fun dropDownSetting() {
-        // 드롭다운 지역 아이템 설정
-        val regionArray= resources.getStringArray(R.array.lecture_location_list)
-        val arrayAdapter=ArrayAdapter<String>(this,R.layout.lecture_dropdown_item, regionArray)
-        binding.dropdownMenu.setAdapter(arrayAdapter)
-
-        // 드롭다운 범위 아이템 설정
-        val distanceList= mutableListOf<String>()
-        distanceList.add("")
-        for(i in 10 until 300 step 10) {
-            distanceList.add(i.toString())
-        }
-        for(i in 300 until 2000 step 100) {
-            distanceList.add(i.toString())
-        }
-        for(i in 2000..20000 step 1000) {
-            distanceList.add(i.toString())
-        }
-        val distanceAdapter=ArrayAdapter<String>(this,R.layout.lecture_dropdown_item,distanceList)
-        binding.dropdownMenu2.setAdapter(distanceAdapter)
     }
 }
