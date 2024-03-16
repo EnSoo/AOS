c3359ca77e1dc2e14fb2a83f197aaeec5eac86ba
backt <backtrackrt5@gmail.com>
2024-03-16 09:27:43 +0900

    MapActivity 남은 작업  1. 현재 위치에서 검색  2. Zoom레벨에 따라 검색 되도록 수정 필요 << 방법을 찾지 못함

--- app/src/main/java/com/mrhiles/aos/fragments/BottomHomeFragment.kt
+++ app/src/main/java/com/mrhiles/aos/fragments/BottomHomeFragment.kt
@@ -1,12 +1,16 @@
 package com.mrhiles.aos.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.gson.Gson
 import com.mrhiles.aos.activities.MainActivity
+import com.mrhiles.aos.activities.MapActivity
 import com.mrhiles.aos.adapter.TapHomeViewPagerAdapter
 import com.mrhiles.aos.databinding.FragmentBottomHomeBinding
 
@@ -36,6 +40,17 @@ class BottomHomeFragment : Fragment(){
             searchQuery=binding.inputEditer.text.toString()
             ma.SearchStudyRoom("${searchQuery} 스터디룸|스터디카페",1000,1,"accuracy")
             false
+        }
+        binding.locationSearch.setOnClickListener {
+            //맵 전체 보여주기
+            val intent= Intent(context, MapActivity::class.java)
+
+            //StudyRoom List에 대한 데이터를 보내기
+            val gson= Gson()
+            val s=gson.toJson(ma.searchStudyRoomResponse!!.documents)
+            intent.putExtra("studyRoom",s)
+            intent.putExtra("type","list") // Type이 Item일 경우 1개만 검색
+            requireContext().startActivity(intent)
         }
     }
 }
